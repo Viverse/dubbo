c03b07cedd5d9a7dd28e22da0c175766064e36df
Viverse <alonelovevain@outlook.com>
2020-06-11 10:30:38 +0800

    11111

--- dubbo_provider/src/main/java/cn/hunan/service/impl/HelloServiceImpl.java
+++ dubbo_provider/src/main/java/cn/hunan/service/impl/HelloServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
+        System.out.println("执行了sayHello方法");
+        System.out.println("我需要进行回退版本测试111");
+        System.out.println("提交新版本测试");
         return "Hello " + name;
     }
 }
